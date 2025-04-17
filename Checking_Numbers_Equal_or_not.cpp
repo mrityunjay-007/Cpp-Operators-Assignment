babd609de7b520d028bcf36172acb6948995391d
mrityunjay-007 <mrityunjaynishad044@gmail.com>
2025-04-17 23:25:55 +0530

    solved assignment ques 1 files are uploaded

---
+++ Checking_Numbers_Equal_or_not.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter the 1st Number: "<<endl;
+    cin>>a;
+    cout<<"Enter the 2nd Number: "<<endl;
+    cin>>b;
+
+   bool c = (a==b)?true:false;
+   cout<<c<<endl;
+
+    return 0;
+}
